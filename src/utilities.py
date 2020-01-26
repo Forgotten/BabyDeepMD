22d7447628da0937e186921ae2fa866bc87a7d42
Leo <lzepeda@lbl.gov>
2020-01-26 16:46:09 -0600

    added exact kernel with other non-exact kernels (no window)

--- src/utilities.py
+++ src/utilities.py
@@ -343,9 +343,98 @@ class pyramidLayerNoBias(tf.keras.layers.Layer):
         x = self.actfn(tf.matmul(x, ker))
     return x
 
+
 class fmmLayer(tf.keras.layers.Layer):
-  def __init__(self, Ncells, Np, width_tran = 0.5, width = 3):
+  # this layers uses a few kernels to approximate exp(-mu)
+  def __init__(self, Ncells, Np):
     super(fmmLayer, self).__init__()
+    self.Ncells = Ncells
+    self.Np = Np
+
+
+  def build(self, input_shape):
+    self.std = []
+    for ii in range(4):
+      self.std.append(self.add_weight("std_"+str(ii),
+                       initializer=tf.initializers.ones(),
+                       shape=[1,]))
+
+    self.bias = []
+    for ii in range(4):
+      self.bias.append(self.add_weight("bias_"+str(ii),
+                       initializer=tf.initializers.zeros(),
+                       shape=[1,]))
+
+
+  @tf.function
+  def call(self, input):
+    ExtCoords =  genDistLongRangeFull(input, self.Ncells, self.Np, 
+                                      [0.0, 0.0], [1.0, 1.0]) # this are hard coded
+    
+    kernelApp = []
+
+    kernelApp.append(tf.abs(self.std[0])*(
+                     tf.expand_dims(tf.math.reciprocal(ExtCoords), axis = -1) - self.bias[0]))
+    kernelApp.append(tf.abs(self.std[1])*(
+                     tf.expand_dims(tf.sqrt(tf.math.reciprocal(ExtCoords)), axis = -1)- self.bias[1]))
+    kernelApp.append(tf.abs(self.std[2])*(
+                     tf.expand_dims(tf.square(tf.math.reciprocal(ExtCoords)), axis = -1)- self.bias[2]))
+    kernelApp.append(tf.abs(self.std[3])*(
+                     tf.expand_dims(tf.math.bessel_i0e(ExtCoords), axis = -1)- self.bias[3]))
+
+    fmm = tf.reduce_sum(tf.concat(kernelApp, axis = -1), axis = 2)
+
+    return fmm 
+
+
+class fmmLayerExact(tf.keras.layers.Layer):
+  # this layers uses a few kernels to approximate exp(-mu)
+  # and we add the exact mu to check if that becomes worse
+  def __init__(self, Ncells, Np, mu):
+    super(fmmLayerExact, self).__init__()
+    self.Ncells = Ncells
+    self.Np = Np 
+    self.mu = mu
+
+  def build(self, input_shape):
+    self.std = []
+    for ii in range(4):
+      self.std.append(self.add_weight("std_"+str(ii),
+                       initializer=tf.initializers.ones(),
+                       shape=[1,]))
+
+    self.bias = []
+    for ii in range(4):
+      self.bias.append(self.add_weight("bias_"+str(ii),
+                       initializer=tf.initializers.zeros(),
+                       shape=[1,]))
+
+
+  @tf.function
+  def call(self, input):
+    ExtCoords =  genDistLongRangeFull(input, self.Ncells, self.Np, 
+                                      [0.0, 0.0], [1.0, 1.0]) # this are hard coded
+    
+    kernelApp = []
+
+    kernelApp.append(tf.abs(self.std[0])*(
+                     tf.expand_dims(tf.math.reciprocal(ExtCoords), axis = -1) - self.bias[0]))
+    kernelApp.append(tf.abs(self.std[1])*(
+                     tf.expand_dims(tf.sqrt(tf.math.reciprocal(ExtCoords)), axis = -1)- self.bias[1]))
+    kernelApp.append(tf.abs(self.std[2])*(
+                     tf.expand_dims(tf.square(tf.math.reciprocal(ExtCoords)), axis = -1)- self.bias[2]))
+    kernelApp.append(tf.abs(self.std[3])*(
+                     tf.expand_dims(tf.math.exp(-self.mu*ExtCoords), axis = -1)- self.bias[3]))
+
+    fmm = tf.reduce_sum(tf.concat(kernelApp, axis = -1), axis = 2)
+
+    return fmm 
+
+class fmmLayerWindow(tf.keras.layers.Layer):
+  # in this case we window the close and long range using a window
+  # we need a better window
+  def __init__(self, Ncells, Np, width_tran = 0.5, width = 3):
+    super(fmmLayerWindow, self).__init__()
     self.Ncells = Ncells
     self.Np = Np
     self.width_tran = width_tran 
