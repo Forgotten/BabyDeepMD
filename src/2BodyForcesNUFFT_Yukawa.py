9d1483f12db8339fce67ce4003f5ba758ad36b2a
Leonardo <lzepeda@math.mit.edu>
2020-03-08 16:54:47 -0500

    implemented NUFFT

--- src/2BodyForcesNUFFT_Yukawa.py
+++ src/2BodyForcesNUFFT_Yukawa.py
@@ -381,21 +381,19 @@ for cycle, (epochs, batchSizeL) in enumerate(zip(Nepochs, batchSizeArray)):
 
 # print( "values of the trainable decay %f" %(model.NUFFTLayer.mu.numpy()))
 
-# ##### testing ######
-# pointsTest, \
-# potentialTest, \
-# forcesTest  = gen_data(Ncells, Np, mu, 1000, minDelta, Lcell)
+##### testing ######
+pointsTest, \
+potentialTest, \
+forcesTest  = gen_data(Ncells, Np, mu, 1000, minDelta, Lcell)
 
-# potentialTestRscl = potentialTest - potMean
-# potentialTestRscl /= potStd
-# forcesTestRscl = forcesTest/potStd
+potentialTestRscl = potentialTest - potMean
+potentialTestRscl /= potStd
+forcesTestRscl = forcesTest/potStd
 
-# potPred, forcePred = model(pointsTest)
-# err = tf.sqrt(tf.reduce_sum(tf.square(potPred - potentialTestRscl)))/tf.sqrt(tf.reduce_sum(tf.square(potPred)))
-# print("Relative Error in the potential is " + str(err.numpy()))
+forcePred = model(pointsTest)
 
-# err = tf.sqrt(tf.reduce_sum(tf.square(forcePred - forcesTestRscl)))/tf.sqrt(tf.reduce_sum(tf.square(forcePred)))
-# print("Relative Error in the forces is " +str(err.numpy()))
+err = tf.sqrt(tf.reduce_sum(tf.square(forcePred - forcesTestRscl)))/tf.sqrt(tf.reduce_sum(tf.square(forcePred)))
+print("Relative Error in the forces is " +str(err.numpy()))
 
 
 # with tf.GradientTape() as tape:
