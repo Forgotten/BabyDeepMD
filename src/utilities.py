a6ee3fbd10a845d9e4d2351bae7c985121d3f67a
Leonardo <lzepeda@math.mit.edu>
2020-03-19 14:53:35 -0700

    added the trainable long range FMM with periodic boundary conditions

--- src/utilities.py
+++ src/utilities.py
@@ -192,7 +192,9 @@ def genDistInvLongRange(Rin, Ncells, Np, av = [0.0, 0.0], std = [1.0, 1.0]):
     return absInvList
 
 @tf.function
-def genDistLongRangeFull(Rin, Ncells, Np, av = [0.0, 0.0], std = [1.0, 1.0]):
+def genDistLongRangeFull(Rin, Ncells, Np, \
+                         av  = [0.0, 0.0], \
+                         std = [1.0, 1.0]):
     # function to generate the generalized coordinates
     # the input has dimensions (Nsamples, Ncells*Np)
     # the ouput has dimensions (Nsamples*Ncells*Np*(3*Np-1), 2)
@@ -212,6 +214,80 @@ def genDistLongRangeFull(Rin, Ncells, Np, av = [0.0, 0.0], std = [1.0, 1.0]):
                 #                      tf.subtract(R[:,i+l,k], R[:,i,j]),-1))-av[1])/std[1] )
           absArray.append((tf.abs(tf.expand_dims(
                             tf.subtract(R[:,j], R[:,i]),-1))-av[0])/std[0])
+      
+      absArrayGlobal.append(tf.expand_dims(
+                                tf.concat(absArray, axis = 1), 1))
+
+
+    # concatenating the lists of tensors to a large tensor 
+    # we don't perform a reduction in the coordinates
+    absInvList = tf.concat(absArrayGlobal, axis = 1)
+    # (batchSize, Ncells*Np, Ncells*Np -1 ) 
+
+    return absInvList
+
+@tf.function
+def genDistInvPerLongRange(Rin, Ncells, Np, L,\
+                           av = [0.0, 0.0], \
+                           std = [1.0, 1.0]):
+    # function to generate the generalized coordinates
+    # here we use the periodic distance
+    # the input has dimensions (Nsamples, Ncells*Np)
+    # the ouput has dimensions (Nsamples*Ncells*Np*(3*Np-1), 2)
+
+    # add assert with respect to the input shape 
+    Nsamples = Rin.shape[0]
+    R = tf.reshape(Rin, (Nsamples, Ncells*Np))
+
+    Abs_Array = []
+    Abs_Inv_Array = []
+    # we compute the difference between points in the same
+    # interaction list
+    for i in range(Ncells*Np):
+      absInvArray = []
+      for j in range(Ncells*Np):
+        if i != j :
+                # absDistArray.append((tf.abs(tf.expand_dims(
+                #                      tf.subtract(R[:,i+l,k], R[:,i,j]),-1))-av[1])/std[1] )
+          a = tf.subtract(R[:,j], R[:,i]) 
+          b = a - L*tf.round(a/L)
+          absInvArray.append((tf.abs(tf.math.reciprocal(
+                            tf.expand_dims(b,-1)))-av[0])/std[0])
+      
+      Abs_Inv_Array.append(tf.expand_dims(
+                                tf.concat(absInvArray, axis = 1), 1))
+
+
+    # concatenating the lists of tensors to a large tensor
+    absInvList = tf.reduce_sum(tf.concat(Abs_Inv_Array, axis = 1), axis = 2)
+
+    return absInvList
+
+@tf.function
+def genDistPerLongRangeFull(Rin, Ncells, Np, L, \
+                            av  = [0.0, 0.0], \
+                            std = [1.0, 1.0]):
+    # function to generate the generalized coordinates
+    # in this case we use the periodic distance
+    # the input has dimensions (Nsamples, Ncells*Np)
+    # the ouput has dimensions (Nsamples*Ncells*Np*(3*Np-1), 2)
+
+    # add assert with respect to the input shape 
+    Nsamples = Rin.shape[0]
+    R = tf.reshape(Rin, (Nsamples, Ncells*Np))
+
+    absArrayGlobal = []
+    # we compute the difference between points in the same
+    # interaction list
+    for i in range(Ncells*Np):
+      absArray = []
+      for j in range(Ncells*Np):
+        if i != j :
+                # absDistArray.append((tf.abs(tf.expand_dims(
+                #                      tf.subtract(R[:,i+l,k], R[:,i,j]),-1))-av[1])/std[1] )
+          a = tf.subtract(R[:,j], R[:,i]) 
+          b = a - L*tf.round(a/L)
+          absArray.append((tf.abs(tf.expand_dims(b,-1))-av[0])/std[0])
       
       absArrayGlobal.append(tf.expand_dims(
                                 tf.concat(absArray, axis = 1), 1))
@@ -512,6 +588,63 @@ class fmmLayerExpTrainable(tf.keras.layers.Layer):
   def call(self, input):
     ExtCoords =  genDistLongRangeFull(input, self.Ncells, self.Np, 
                                       [0.0, 0.0], [1.0, 1.0]) # this are hard coded
+    
+    kernelApp = []
+
+    kernelApp.append(tf.abs(self.std[0])*(
+                     tf.expand_dims(tf.math.reciprocal(ExtCoords), axis = -1) - self.bias[0]))
+    kernelApp.append(tf.abs(self.std[1])*(
+                     tf.expand_dims(tf.sqrt(tf.math.reciprocal(ExtCoords)), axis = -1)- self.bias[1]))
+    kernelApp.append(tf.abs(self.std[2])*(
+                     tf.expand_dims(tf.square(tf.math.reciprocal(ExtCoords)), axis = -1)- self.bias[2]))
+    kernelApp.append(tf.abs(self.std[3])*(
+                     tf.expand_dims(tf.math.exp(-self.mu*ExtCoords), axis = -1)- self.bias[3]))
+
+    fmm = tf.reduce_sum(tf.concat(kernelApp, axis = -1), axis = 2)
+
+    return fmm 
+
+
+class fmmLayerExpTrainablePer(tf.keras.layers.Layer):
+  # this layers uses a few kernels to approximate exp(-mu)
+  # we add a wrong mu, but we let it evolve freely.
+  # in this case we use the periodic distance
+  def __init__(self, Ncells, Np, L):
+    super(fmmLayerExpTrainablePer, self).__init__()
+    # number of cells,
+    self.Ncells = Ncells
+    # number of points per cell
+    self.Np = Np 
+    # lenght of the domain 
+    self.L = L
+
+  def build(self, input_shape):
+
+    # we let the mu to be trained
+
+    self.mu = self.add_weight("mu",
+                       initializer=tf.initializers.ones(),
+                       shape=[1,])
+
+    self.std = []
+    for ii in range(4):
+      self.std.append(self.add_weight("std_"+str(ii),
+                       initializer=tf.initializers.ones(),
+                       shape=[1,]))
+
+    self.bias = []
+    for ii in range(4):
+      self.bias.append(self.add_weight("bias_"+str(ii),
+                       initializer=tf.initializers.zeros(),
+                       shape=[1,]))
+
+
+  @tf.function
+  def call(self, input):
+    ExtCoords =  genDistPerLongRangeFull(input, self.Ncells, 
+                                         self.Np, self.L,
+                                         [0.0, 0.0], 
+                                         [1.0, 1.0]) # this are hard coded
     
     kernelApp = []
 
@@ -1143,6 +1276,135 @@ class NUFFTLayerMultiChannelInit(tf.keras.layers.Layer):
     #mult = 
 
     return fmm 
+
+
+class RNUFFTLayerMultiChannelInit(tf.keras.layers.Layer):
+  # this layers uses two multipliers to approximate a 
+  # periodic potential 
+  # this uses only an rfft thus imposing the ouput to be real
+  def __init__(self, nChannels, NpointsMesh, tau, xLims, mu0 = 1.0):
+    super(RNUFFTLayerMultiChannelInit, self).__init__()
+    self.nChannels = nChannels
+    self.NpointsMesh = NpointsMesh 
+    self.mu0 = tf.constant(mu0, dtype=tf.float32)
+
+    # we need the number of points to be odd 
+    assert NpointsMesh % 2 == 1
+
+    
+    self.xLims = xLims
+    self.L = np.abs(xLims[1] - xLims[0])
+    self.tau = tf.constant(12*(self.L/(2*np.pi*NpointsMesh))**2, dtype = tf.float32)# the size of the mollifications
+    self.kGrid = tf.constant((2*np.pi/self.L)*np.linspace(-(NpointsMesh//2), 
+                                                            NpointsMesh//2, 
+                                                            NpointsMesh), 
+                              dtype = tf.float32)
+    # we need to define a mesh betwen xLims[0] and xLims[1]
+    self.xGrid =  tf.constant(np.linspace(xLims[0], 
+                                          xLims[1], 
+                                          NpointsMesh+1)[:-1], 
+                              dtype = tf.float32)
+
+
+  def build(self, input_shape):
+
+    print("building the channels")
+    # we initialize the channel multipliers
+    # we need to add a parametrized family in here
+
+    xExp = tf.expand_dims(4*np.pi*tf.math.reciprocal(tf.square(self.kGrid) + \
+                                  tf.square(self.mu0)), 0)
+
+    initKExp = tf.keras.initializers.Constant(xExp.numpy())
+
+    xExp2 = tf.expand_dims(4*np.pi*tf.math.reciprocal(tf.square(self.kGrid) + \
+                                  tf.square(1.0)), 0)
+
+    initKExp2 = tf.keras.initializers.Constant(xExp2.numpy())
+
+    self.multipliersRe = []
+    self.multipliersIm = []
+
+    self.multipliersRe.append(self.add_weight("multRe_0",
+                       initializer=initKExp, shape = (1, self.NpointsMesh)))
+    self.multipliersIm.append(self.add_weight("multIm_0",
+                       initializer=tf.initializers.zeros(), 
+                       shape = (1, self.NpointsMesh)))
+
+    self.multipliersRe.append(self.add_weight("multRe_1",
+                       initializer=initKExp2, shape = (1, self.NpointsMesh)))
+    self.multipliersIm.append(self.add_weight("multIm_1",
+                       initializer=tf.initializers.zeros(), 
+                        shape = (1, self.NpointsMesh)))
+
+
+    # this needs to be properly initialized it, otherwise it won't even be enough
+
+  @tf.function
+  def call(self, input):
+    # we need to add an iterpolation step
+    # this needs to be perodic distance!!!
+    # (batch_size, Np*Ncells)
+    diff = tf.expand_dims(input, -1) - tf.reshape(self.xGrid, (1,1, self.NpointsMesh))
+    # (batch_size, Np*Ncells, NpointsMesh)
+    # we compute all the localized gaussians
+    array_gaussian = gaussianPer(diff, self.tau)
+    # we add them together
+    arrayReducGaussian = tf.complex(tf.reduce_sum(array_gaussian, axis = 1), 0.0)
+    # (batch_size, NpointsMesh) (we sum the gaussians together)
+    # we apply the fft
+    print("computing the FFT")
+
+    fftGauss = tf.signal.fftshift(tf.signal.fft(arrayReducGaussian))
+    #(batch_size, NpointsMesh)
+    Deconv = tf.complex(tf.expand_dims(gaussianDeconv(self.kGrid, self.tau), 0),0.0)
+    #(1, NpointsMesh)
+
+    rfft = tf.multiply(fftGauss, Deconv)/(2*np.pi*self.NpointsMesh/self.L)
+    #(batch_size, NpointsMesh)
+    # we are only using one channel
+    #rfft = tf.expand_dims(rfftDeconv, 1)
+    # Fourier multipliers
+
+    Rerfft = tf.math.real(rfft)
+    Imrfft = tf.math.imag(rfft)
+
+    print("applying the multipliers")
+
+    # multfft = tf.multiply(self.multChannels*rfft)
+    multReRefft = tf.multiply(self.multipliersRe[0],Rerfft)
+    multReImfft = tf.multiply(self.multipliersIm[0],Rerfft)
+    multImImfft = tf.multiply(self.multipliersRe[0],Imrfft)
+    multImRefft = tf.multiply(self.multipliersIm[0],Imrfft)
+
+    multfft = tf.expand_dims(tf.complex(multReRefft-multImImfft, \
+                                        multReImfft+multImRefft),1)
+
+    # multfft = tf.multiply(self.multChannels*rfft)
+    multReRefft2 = tf.multiply(self.multipliersRe[1],Rerfft)
+    multReImfft2 = tf.multiply(self.multipliersIm[1],Rerfft)
+    multImImfft2 = tf.multiply(self.multipliersRe[1],Imrfft)
+    multImRefft2 = tf.multiply(self.multipliersIm[1],Imrfft)
+
+    multfft2 = tf.expand_dims(tf.complex(multReRefft2-multImImfft2, \
+                          multReImfft2+multImRefft2), 1)
+
+    multFFT = tf.concat([multfft, multfft2], axis = 1)
+
+
+    multfftDeconv = tf.multiply(multFFT, tf.expand_dims(Deconv,1))
+
+    print(multfft.shape)
+    print("inverse fft")
+    irfft = tf.math.real(tf.expand_dims(tf.signal.ifft(tf.signal.ifftshift(multfftDeconv)), 1))
+
+    local = irfft*tf.expand_dims(array_gaussian, 2)
+    
+    fmm = tf.reduce_sum(local, axis = -1)/(2*np.pi*self.NpointsMesh/self.L)
+    #mult = 
+
+    return fmm 
+
 
 
 class testInit(tf.keras.layers.Layer):
