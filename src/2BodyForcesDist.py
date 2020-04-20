085c050ef307813ec84a67ecf9beb33a06b39bfc
Leonardo <lzepeda@math.mit.edu>
2020-04-19 23:21:51 -0500

    modified the rescaling

--- src/2BodyForcesDist.py
+++ src/2BodyForcesDist.py
@@ -157,7 +157,9 @@ Npoints = Np*Ncells
 
 
 genCoordinates = genDistInvPerNlist(Rin, Npoints, neighList, L)
-numNonZero =  tf.reduce_sum(tf.cast(genCoordinates>0, tf.int32), axis = 0).numpy()[0]
+filter = tf.cast(tf.abs(genCoordinates)>0, tf.int32)
+numNonZero =  tf.reduce_sum(filter, axis = 0).numpy()[0]
+numTotal = genCoordinates.shape[0]  
 
 if loadFile: 
   # if we are loadin a file we need to be sure that we are 
@@ -168,10 +170,10 @@ if loadFile:
 else:
   av = tf.reduce_sum(genCoordinates, 
                     axis = 0, 
-                    keepdims =True ).numpy()[0]/numNonZero
-  std = np.sqrt(tf.reduce_sum(tf.square(genCoordinates - av), 
+                    keepdims =True).numpy()[0]/numNonZero
+  std = np.sqrt((tf.reduce_sum(tf.square(genCoordinates - av), 
                              axis = 0, 
-                             keepdims=True).numpy()[0]/numNonZero)
+                             keepdims=True).numpy()[0] - av**2*(numTotal-numNonZero)) /numNonZero)
 
 print("mean of the inputs are %.8f and %.8f"%(av[0], av[1]))
 print("std of the inputs are %.8f and %.8f"%(std[0], std[1]))
