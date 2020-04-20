ce4decfbf6ffe9585332f74b190af9eb8e06d6ef
Leonardo <lzepeda@math.mit.edu>
2020-04-19 19:13:19 -0500

    first version of the list based Baby Deep MD

--- src/utilities.py
+++ src/utilities.py
@@ -830,6 +830,32 @@ def train_step_2(model, optimizer, loss,
 
   return total_loss
 
+@tf.function
+def trainStepList(model, optimizer, loss,
+                    inputs, neighList, outputsE, outputsF, 
+                    weightE, weightF):
+# funtion to perform one training step when predicting both the
+# potential and the forces
+  with tf.GradientTape() as tape:
+    # we use the model the predict the outcome
+    predE, predF = model(inputs, neighList, training=True)
+
+    # fidelity loss usin mse
+    lossE = loss(predE, outputsE)
+    lossF = loss(predF, outputsF)/outputsF.shape[-1]
+
+    if weightF > 0.0:
+      total_loss = weightE*lossE + weightF*lossF
+    else: 
+      total_loss = weightE*lossE 
+
+  # compute the gradients of the total loss with respect to the trainable variables
+  gradients = tape.gradient(total_loss, model.trainable_variables)
+  # update the parameters of the network
+  optimizer.apply_gradients(zip(gradients, model.trainable_variables))
+
+  return total_loss
+
 def computeWeight(weightInit, weightLimit, lrT, lr0):
   return np.abs(weightLimit*(1-lrT/lr0) + weightInit*(lrT/lr0))
 
@@ -1172,6 +1198,142 @@ class NUFFTLayerMultiChannel(tf.keras.layers.Layer):
 
     return fmm 
 
+class NUFFTLayerMultiChannelOneSided(tf.keras.layers.Layer):
+  # this layers uses a few kernels to approximate exp(-mu)
+  # and we add the exact mu to check if that becomes worse
+  def __init__(self, nChannels, NpointsMesh, sigma, xLims, mu0 = 1.0):
+    super(NUFFTLayerMultiChannelOneSided, self).__init__()
+    self.nChannels = nChannels
+    self.NpointsMesh = NpointsMesh 
+    self.mu0 = tf.constant(mu0, dtype=tf.float32)
+
+    # we need the number of points to be odd 
+    assert NpointsMesh % 2 == 1
+
+    
+    self.xLims = xLims
+    self.L = np.abs(xLims[1] - xLims[0])
+    # this one needs to be a one-dimensional numpy array 
+    self.sigma = sigma
+    self.tau = tf.constant(12*(self.L/(2*np.pi*NpointsMesh))**2, 
+                           dtype = tf.float32)# the size of the mollifications
+    self.kGrid = tf.constant((2*np.pi/self.L)*\
+                              np.linspace(-(NpointsMesh//2), 
+                                            NpointsMesh//2, 
+                                            NpointsMesh), 
+                              dtype = tf.float32)
+    # we need to define a mesh betwen xLims[0] and xLims[1]
+    self.xGrid =  tf.constant(np.linspace(xLims[0], 
+                                          xLims[1], 
+                                          NpointsMesh+1)[:-1], 
+                              dtype = tf.float32)
+
+
+  def build(self, input_shape):
+
+    initSigma = tf.keras.initializers.Constant([self.sigma])
+
+    self.sigmaVar = self.add_weight("sigma",
+                       initializer=initSigma, shape = (1,))
+
+    ## TODO: add the correct parametrization 
+    print("building the channels")
+    # we initialize the channel multipliers
+    # we need to add a parametrized family in here
+    self.shift = []
+    for ii in range(self.nChannels):
+      self.shift.append(self.add_weight("std_"+str(ii),
+                       initializer=tf.initializers.ones(),
+                       shape=[1,]))
+
+    self.amplitud = []
+    for ii in range(self.nChannels):
+      self.amplitud.append(self.add_weight("bias_"+str(ii),
+                       initializer=tf.initializers.ones(),
+                       shape=[1,]))
+
+
+
+    # this needs to be properly initialized it, otherwise it won't even be enough
+
+  @tf.function
+  def call(self, input):
+    # we need to add an iterpolation step
+    # this needs to be perodic distance!!!
+    # (batch_size, Np*Ncells)
+    diff = tf.expand_dims(input, -1) - tf.reshape(self.xGrid, (1,1, self.NpointsMesh))
+    # (batch_size, Np*Ncells, NpointsMesh)
+    # we compute all the localized gaussians
+    array_gaussian = gaussian(diff, self.tau)
+    # we add them together
+    array_normal   = normalPer(diff, self.sigmaVar, self.L)
+    # we add them together
+    arrayReducGaussian = tf.complex(tf.reduce_sum(array_normal, axis = 1), 0.0)
+
+    print("computing the FFT") 
+
+    fftGauss = tf.signal.fftshift(tf.signal.fft(arrayReducGaussian))
+    #(batch_size, NpointsMesh)
+    Deconv = tf.complex(tf.expand_dims(gaussianDeconv(self.kGrid, self.tau), 0),0.0)
+    #(1, NpointsMesh)
+    #(batch_size, NpointsMesh)
+    # we are only using one channel
+    #rfft = tf.expand_dims(rfftDeconv, 1)
+    # Fourier multipliers
+
+    Rerfft = tf.math.real(fftGauss)
+    Imrfft = tf.math.imag(fftGauss)
+
+    print("applying the multipliers")
+
+ 
+    multiplier = tf.expand_dims(-self.amplitud[0]*4*np.pi*\
+                                  tf.math.reciprocal(tf.square(self.kGrid) + \
+                                  tf.square(5*self.shift[0])), 0)
+      
+    multiplierRe = tf.math.real(multiplier)
+    multiplierIm = tf.math.imag(multiplier)
+
+    # multfft = tf.multiply(self.multChannels*rfft)
+    multReRefft = tf.multiply(multiplierRe,Rerfft)
+    multReImfft = tf.multiply(multiplierIm,Rerfft)
+    multImImfft = tf.multiply(multiplierIm,Imrfft)
+    multImRefft = tf.multiply(multiplierRe,Imrfft)
+
+    multfft = tf.expand_dims(tf.complex(multReRefft-multImImfft, \
+                                        multReImfft+multImRefft),1)
+
+    multiplier2 = tf.expand_dims(self.amplitud[1]*4*np.pi*\
+                                  tf.math.pow(tf.math.reciprocal(tf.square(self.kGrid) + \
+                                  tf.square(5*self.shift[1])), 2), 0) 
+      
+    multiplierRe2 = tf.math.real(multiplier2)
+    multiplierIm2 = tf.math.imag(multiplier2)
+
+    # multfft = tf.multiply(self.multChannels*rfft)
+    multReRefft2 = tf.multiply(multiplierRe2,Rerfft)
+    multReImfft2 = tf.multiply(multiplierIm2,Rerfft)
+    multImImfft2 = tf.multiply(multiplierIm2,Imrfft)
+    multImRefft2 = tf.multiply(multiplierRe2,Imrfft)
+
+    multfft2 = tf.expand_dims(tf.complex(multReRefft2-multImImfft2, \
+                          multReImfft2+multImRefft2), 1)
+
+    multFFT = tf.concat([multfft, multfft2], axis = 1)
+
+
+    multfftDeconv = tf.multiply(multFFT, tf.expand_dims(Deconv,1))
+
+    print(multfft.shape)
+    print("inverse fft")
+    irfft = tf.math.real(tf.expand_dims(tf.signal.ifft(tf.signal.ifftshift(multfftDeconv)), 1))
+
+    local = irfft*tf.expand_dims(array_gaussian, 2)
+    
+    fmm = tf.reduce_sum(local, axis = -1)/self.NpointsMesh
+    #mult = 
+
+    return fmm 
 
 
 class NUFFTLayerMultiChannelInit(tf.keras.layers.Layer):
@@ -1499,4 +1661,124 @@ def gaussianDeconv(k, tau):
   return tf.sqrt(np.pi/tau)*tf.exp(tf.square(k)*tau)
 
 
+############### variable number of particules per cell 
 
+# write bining function input Rin, Ncells, L, max number particules per cell
+# ouput the interaction list of each of the atoms involved
+
+# loop over each atom, and compute the distances in tensorflow. 
+# if the index of the interaction is -1 (zeros won't work here)
+# add a zero in that point 
+
+# make the 
+
+# neigh List has a fixed lenght
+
+@tf.function
+def genDistInvPerNlist(Rin, Npoints, neighList, L, av = [0.0, 0.0], std = [1.0, 1.0]):
+    # function to generate the generalized coordinates for periodic data
+    # the input has dimensions (Nsamples, Ncells*Np)
+    # the ouput has dimensions (Nsamples*Ncells*Np*(3*Np-1), 2)
+
+    # neighList is a (Nsample, Npoints, maxNeigh)
+
+    # add assert with respect to the input shape 
+    Nsamples = Rin.shape[0]
+    maxNumNeighs = neighList.shape[-1]
+    R2 = tf.reshape(Rin, (Nsamples, Npoints))
+
+    Abs_Array = []
+    Abs_Inv_Array = []
+    # we compute the difference between points in the same
+    # interaction list
+
+    Idx1 = tf.constant(np.linspace(0,Nsamples-1, Nsamples).astype(np.int64).reshape((-1,1)))
+
+    for r in range(Npoints):
+        absDistArrayPoint = []
+        absInvArrayPoint =[]
+        for j in range(maxNumNeighs) :
+            # we consider the indices and the concatenat with the other set
+            # in order to have a matrix having the coordinates in Rin
+            Idx = tf.concat([Idx1, tf.reshape(neighList[:,r,j],(-1,1))], axis = 1) 
+            # we create a filter for the unwanted data points 
+            filter = tf.cast(Idx > -1, tf.int64)     
+            # we substract
+            a = tf.subtract(Rin[:,r], tf.gather_nd(Rin, Idx*filter)) 
+            # apply filter         
+            a = tf.multiply(a, tf.cast(filter[:,1], tf.float32))
+            # applying the periodicity
+            b = a - L*tf.round(a/L)
+
+            # we apply the % TODO check when is the best place for the normalization
+            bnorm = (tf.abs(tf.expand_dims(b,-1)) -av[1])/std[1] 
+            # we need to smear it a little bit 
+            binv = (tf.abs(tf.math.reciprocal(tf.expand_dims(b,-1)-0.000001)) -av[0])/std[0]
+
+            # we filtered out the padding
+            bnormFilt = tf.multiply(bnorm,
+                                    tf.reshape(tf.cast(filter[:,1], tf.float32),(-1,1))) 
+            binvFilt = tf.multiply(binv,
+                                    tf.reshape(tf.cast(filter[:,1], tf.float32),(-1,1)))
+
+            absDistArrayPoint.append(bnormFilt)
+            absInvArrayPoint.append(binvFilt)
+        Abs_Array.append(tf.expand_dims(
+                         tf.concat(absDistArrayPoint, axis = 1), 1))
+        Abs_Inv_Array.append(tf.expand_dims(
+                             tf.concat(absInvArrayPoint, axis = 1), 1))
+
+    # concatenating the lists of tensors to a large tensor
+    absDistList = tf.concat(Abs_Array, axis = 1)
+    absInvList = tf.concat(Abs_Inv_Array, axis = 1)
+
+
+    # R_Diff_abs = absDistList)-
+    # R_Diff_inv = (tf.abs(absInvList)-av[0])/std[0]
+    # or using the reciprocal 
+    #R_Diff_abs = tf.math.reciprocal(tf.abs(absDistList) + 0.00001)
+
+    R_Diff_total = tf.concat([tf.reshape(absInvList, (-1,1)), 
+                              tf.reshape(absDistList, (-1,1))], axis = 1)
+
+    # asserting the final size of the tensor
+    # assert R_Diff_total.shape[0] == Nsamples*Ncells*Np*(3*Np-1)
+
+    return R_Diff_total
+
+
+def computInterList( Rinnumpy, L,  radious, maxNumNeighs):
+  Nsamples, Npoints = Rinnumpy.shape
+
+  # computing the distance
+  DistNumpy = np.abs(Rinnumpy.reshape(Nsamples,Npoints,1) \
+              - Rinnumpy.reshape(Nsamples,1,Npoints))
+
+  # periodicing the distance
+  DistNumpy = DistNumpy - L*np.round(DistNumpy/L)
+
+  # loop to get the proper indices and padding the rest
+  Idex = []
+  for ii in range(0,Nsamples):
+    sampleIdx = []
+    for jj in range(0, Npoints):
+      localIdx = []
+      for kk in range(0, Npoints):
+        if jj!= kk and np.abs(DistNumpy[ii,jj,kk]) < radious:
+          # checking that we are not going over the mac number of
+          # neighboors, if so we break the loop
+          if len(localIdx) >= maxNumNeighs:
+            print("Number of neighboors is larger than the max number allowed")
+            break
+          localIdx.append(kk)
+
+      while len(localIdx) < maxNumNeighs:
+        localIdx.append(-1)
+      sampleIdx.append(localIdx)
+
+    Idex.append(sampleIdx)
+
+# converting the list of points to an numpy array.
+  Idx = np.array(Idex)
+
+  return Idx
