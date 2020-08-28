21582591fc72ea2c00f0af32177281a18d2edabb
Leonardo <lzepeda@math.mit.edu>
2020-08-27 19:11:36 -0500

    feature: added the 3d generation of the data

--- src/data_gen_3d.py
+++ src/data_gen_3d.py
@@ -96,6 +96,7 @@ def genDataPer3D(Ncells, Np, mu, Nsamples, minDelta = 0.0, Lcell = 0.0):
     return pointsArray, potentialArray, forcesArray
 
 
+
 def gaussian3D(x, y, z, center, tau):
     return (1/np.sqrt(2*np.pi*tau)**3)*\
            np.exp( -0.5*(  np.square(x - center[0]) \
@@ -103,173 +104,173 @@ def gaussian3D(x, y, z, center, tau):
                          + np.square(z - center[2]))/tau**2 )
 
 
-def computeDerPot3DPer(Nx, mu, Ls, xCenter = [0.0, 0.0, 0.0], nPointSmear = 5):   
+# def computeDerPot3DPer(Nx, mu, Ls, xCenter = [0.0, 0.0, 0.0], nPointSmear = 5):   
     
-    xGrid = np.linspace(0, Ls, Nx+1)[:-1] 
-    kGrid = 2*np.pi*np.linspace(-(Nx//2), Nx//2, Nx)/Ls      
+#     xGrid = np.linspace(0, Ls, Nx+1)[:-1] 
+#     kGrid = 2*np.pi*np.linspace(-(Nx//2), Nx//2, Nx)/Ls      
 
-    # creating the 3D space and frequency grids
-    y_grid, x_grid, z_grid = np.meshgrid(xGrid, xGrid, xGrid)
-    ky_grid, kx_grid, kz_grid = np.meshgrid(kGrid, kGrid, kGrid)
+#     # creating the 3D space and frequency grids
+#     y_grid, x_grid, z_grid = np.meshgrid(xGrid, xGrid, xGrid)
+#     ky_grid, kx_grid, kz_grid = np.meshgrid(kGrid, kGrid, kGrid)
 
 
-    filterM = 1#0.5 - 0.5*np.tanh(np.abs(3*kGrid/np.sqrt(Nx)) - np.sqrt(Nx))
-    # multiplier 
-    mult = 4*np.pi*filterM/(  np.square(kx_grid) \
-                            + np.square(ky_grid) \
-                            + np.square(kz_grid) \
-                            + np.square(mu))
+#     filterM = 1#0.5 - 0.5*np.tanh(np.abs(3*kGrid/np.sqrt(Nx)) - np.sqrt(Nx))
+#     # multiplier 
+#     mult = 4*np.pi*filterM/(  np.square(kx_grid) \
+#                             + np.square(ky_grid) \
+#                             + np.square(kz_grid) \
+#                             + np.square(mu))
 
-    # here we smear the dirac delta
-    # we use the width of the smearing for 
-    tau = nPointSmear*Ls/Nx
+#     # here we smear the dirac delta
+#     # we use the width of the smearing for 
+#     tau = nPointSmear*Ls/Nx
 
-    # we impose the periodicity
-    x = gaussian3D(x_grid-Ls, y_grid-Ls, z_grid-Ls, xCenter, tau) + \
-        gaussian3D(x_grid-Ls, y_grid   , z_grid-Ls, xCenter, tau) + \
-        gaussian3D(x_grid-Ls, y_grid+Ls, z_grid-Ls, xCenter, tau) + \
-        gaussian3D(x_grid   , y_grid-Ls, z_grid-Ls, xCenter, tau) + \
-        gaussian3D(x_grid   , y_grid   , z_grid-Ls, xCenter, tau) + \
-        gaussian3D(x_grid   , y_grid+Ls, z_grid-Ls, xCenter, tau) + \
-        gaussian3D(x_grid+Ls, y_grid-Ls, z_grid-Ls, xCenter, tau) + \
-        gaussian3D(x_grid+Ls, y_grid   , z_grid-Ls, xCenter, tau) + \
-        gaussian3D(x_grid+Ls, y_grid+Ls, z_grid-Ls, xCenter, tau) + \
-        gaussian3D(x_grid-Ls, y_grid-Ls, z_grid-Ls, xCenter, tau) + \
-        gaussian3D(x_grid-Ls, y_grid   , z_grid,    xCenter, tau) + \
-        gaussian3D(x_grid-Ls, y_grid+Ls, z_grid,    xCenter, tau) + \
-        gaussian3D(x_grid   , y_grid-Ls, z_grid,    xCenter, tau) + \
-        gaussian3D(x_grid   , y_grid   , z_grid,    xCenter, tau) + \
-        gaussian3D(x_grid   , y_grid+Ls, z_grid,    xCenter, tau) + \
-        gaussian3D(x_grid+Ls, y_grid-Ls, z_grid,    xCenter, tau) + \
-        gaussian3D(x_grid+Ls, y_grid   , z_grid,    xCenter, tau) + \
-        gaussian3D(x_grid+Ls, y_grid+Ls, z_grid,    xCenter, tau) + \
-        gaussian3D(x_grid-Ls, y_grid-Ls, z_grid+Ls, xCenter, tau) + \
-        gaussian3D(x_grid-Ls, y_grid   , z_grid+Ls, xCenter, tau) + \
-        gaussian3D(x_grid-Ls, y_grid+Ls, z_grid+Ls, xCenter, tau) + \
-        gaussian3D(x_grid   , y_grid-Ls, z_grid+Ls, xCenter, tau) + \
-        gaussian3D(x_grid   , y_grid   , z_grid+Ls, xCenter, tau) + \
-        gaussian3D(x_grid   , y_grid+Ls, z_grid+Ls, xCenter, tau) + \
-        gaussian3D(x_grid+Ls, y_grid-Ls, z_grid+Ls, xCenter, tau) + \
-        gaussian3D(x_grid+Ls, y_grid   , z_grid+Ls, xCenter, tau) + \
-        gaussian3D(x_grid+Ls, y_grid+Ls, z_grid+Ls, xCenter, tau) 
+#     # we impose the periodicity
+#     x = gaussian3D(x_grid-Ls, y_grid-Ls, z_grid-Ls, xCenter, tau) + \
+#         gaussian3D(x_grid-Ls, y_grid   , z_grid-Ls, xCenter, tau) + \
+#         gaussian3D(x_grid-Ls, y_grid+Ls, z_grid-Ls, xCenter, tau) + \
+#         gaussian3D(x_grid   , y_grid-Ls, z_grid-Ls, xCenter, tau) + \
+#         gaussian3D(x_grid   , y_grid   , z_grid-Ls, xCenter, tau) + \
+#         gaussian3D(x_grid   , y_grid+Ls, z_grid-Ls, xCenter, tau) + \
+#         gaussian3D(x_grid+Ls, y_grid-Ls, z_grid-Ls, xCenter, tau) + \
+#         gaussian3D(x_grid+Ls, y_grid   , z_grid-Ls, xCenter, tau) + \
+#         gaussian3D(x_grid+Ls, y_grid+Ls, z_grid-Ls, xCenter, tau) + \
+#         gaussian3D(x_grid-Ls, y_grid-Ls, z_grid-Ls, xCenter, tau) + \
+#         gaussian3D(x_grid-Ls, y_grid   , z_grid,    xCenter, tau) + \
+#         gaussian3D(x_grid-Ls, y_grid+Ls, z_grid,    xCenter, tau) + \
+#         gaussian3D(x_grid   , y_grid-Ls, z_grid,    xCenter, tau) + \
+#         gaussian3D(x_grid   , y_grid   , z_grid,    xCenter, tau) + \
+#         gaussian3D(x_grid   , y_grid+Ls, z_grid,    xCenter, tau) + \
+#         gaussian3D(x_grid+Ls, y_grid-Ls, z_grid,    xCenter, tau) + \
+#         gaussian3D(x_grid+Ls, y_grid   , z_grid,    xCenter, tau) + \
+#         gaussian3D(x_grid+Ls, y_grid+Ls, z_grid,    xCenter, tau) + \
+#         gaussian3D(x_grid-Ls, y_grid-Ls, z_grid+Ls, xCenter, tau) + \
+#         gaussian3D(x_grid-Ls, y_grid   , z_grid+Ls, xCenter, tau) + \
+#         gaussian3D(x_grid-Ls, y_grid+Ls, z_grid+Ls, xCenter, tau) + \
+#         gaussian3D(x_grid   , y_grid-Ls, z_grid+Ls, xCenter, tau) + \
+#         gaussian3D(x_grid   , y_grid   , z_grid+Ls, xCenter, tau) + \
+#         gaussian3D(x_grid   , y_grid+Ls, z_grid+Ls, xCenter, tau) + \
+#         gaussian3D(x_grid+Ls, y_grid-Ls, z_grid+Ls, xCenter, tau) + \
+#         gaussian3D(x_grid+Ls, y_grid   , z_grid+Ls, xCenter, tau) + \
+#         gaussian3D(x_grid+Ls, y_grid+Ls, z_grid+Ls, xCenter, tau) 
 
-    xFFT = np.fft.fftshift(np.fft.fftn(x))
+#     xFFT = np.fft.fftshift(np.fft.fftn(x))
     
-    fFFT = xFFT*mult
+#     fFFT = xFFT*mult
     
-    f = np.real(np.fft.ifftn(np.fft.ifftshift(fFFT)))
+#     f = np.real(np.fft.ifftn(np.fft.ifftshift(fFFT)))
 
-    dfdxFFT = 1.j*kx_grid*fFFT
-    dfdyFFT = 1.j*ky_grid*fFFT
-    dfdzFFT = 1.j*kz_grid*fFFT
+#     dfdxFFT = 1.j*kx_grid*fFFT
+#     dfdyFFT = 1.j*ky_grid*fFFT
+#     dfdzFFT = 1.j*kz_grid*fFFT
 
-    dfdx = np.fft.ifftn(np.fft.ifftshift(dfdxFFT))
-    dfdy = np.fft.ifftn(np.fft.ifftshift(dfdyFFT))
-    dfdz = np.fft.ifftn(np.fft.ifftshift(dfdzFFT))
-
-
-    return  x_grid, y_grid, z_grid, f, \
-            np.real(dfdx), np.real(dfdy), np.real(dfdz)
+#     dfdx = np.fft.ifftn(np.fft.ifftshift(dfdxFFT))
+#     dfdy = np.fft.ifftn(np.fft.ifftshift(dfdyFFT))
+#     dfdz = np.fft.ifftn(np.fft.ifftshift(dfdzFFT))
 
 
+#     return  x_grid, y_grid, z_grid, f, \
+#             np.real(dfdx), np.real(dfdy), np.real(dfdz)
 
-def gen_data_yukawa_3D_Per(Ncells, Np, 
-                           sigma, Nsamples, 
-                           minDelta = 0.0, Lcell = 0.0): 
 
-    pointsArray = np.zeros((Nsamples, Np*Ncells**2, 2))
-    potentialArray = np.zeros((Nsamples,1))
-    forcesArray = np.zeros((Nsamples, Np*Ncells**2, 2))
 
-    if Lcell == 0.0 :
-        sizeCell = 1/Ncells
-    else :
-        sizeCell = Lcell
+# def gen_data_yukawa_3D_Per(Ncells, Np, 
+#                            sigma, Nsamples, 
+#                            minDelta = 0.0, Lcell = 0.0): 
 
-    NpointsPerCell = 1000
-    Nx = Ncells*NpointsPerCell + 1
-    Ls = Ncells*sizeCell
+#     pointsArray = np.zeros((Nsamples, Np*Ncells**2, 2))
+#     potentialArray = np.zeros((Nsamples,1))
+#     forcesArray = np.zeros((Nsamples, Np*Ncells**2, 2))
 
-    x_grid, y_grid, z_grid,
-    pot, dpotdx, dpotdy, dpotdz = computeDerPot3DPer(Nx, sigma, Ls)
+#     if Lcell == 0.0 :
+#         sizeCell = 1/Ncells
+#     else :
+#         sizeCell = Lcell
 
-    idxCell = np.linspace(0,NpointsPerCell-1, NpointsPerCell).astype(int)
-    idxStart = np.array([ii*NpointsPerCell for ii in range(Ncells)]).reshape(-1,1)
+#     NpointsPerCell = 1000
+#     Nx = Ncells*NpointsPerCell + 1
+#     Ls = Ncells*sizeCell
+
+#     x_grid, y_grid, z_grid,
+#     pot, dpotdx, dpotdy, dpotdz = computeDerPot3DPer(Nx, sigma, Ls)
+
+#     idxCell = np.linspace(0,NpointsPerCell-1, NpointsPerCell).astype(int)
+#     idxStart = np.array([ii*NpointsPerCell for ii in range(Ncells)]).reshape(-1,1)
     
-    idx_cell_y, idx_cell_x = np.meshgrid(idxCell, idxCell) 
-    idx_start_y, idx_start_x = np.meshgrid(idxStart, idxStart) 
+#     idx_cell_y, idx_cell_x = np.meshgrid(idxCell, idxCell) 
+#     idx_start_y, idx_start_x = np.meshgrid(idxStart, idxStart) 
 
-    idx_point_x = idx_start_x.reshape((Ncells, Ncells, 1)) \
-                      + np.random.choice(idx_cell_x.reshape((-1,)), 
-                                         [Ncells, Ncells, Np])
+#     idx_point_x = idx_start_x.reshape((Ncells, Ncells, 1)) \
+#                       + np.random.choice(idx_cell_x.reshape((-1,)), 
+#                                          [Ncells, Ncells, Np])
 
-    idx_point_y = idx_start_y.reshape((Ncells, Ncells, 1)) \
-                      + np.random.choice(idx_cell_y.reshape((-1,)), 
-                                         [Ncells, Ncells, Np])
+#     idx_point_y = idx_start_y.reshape((Ncells, Ncells, 1)) \
+#                       + np.random.choice(idx_cell_y.reshape((-1,)), 
+#                                          [Ncells, Ncells, Np])
 
-    for i in range(Nsamples):
+#     for i in range(Nsamples):
 
-        # just starting point 
-        dist = [10.0, 10.0]
+#         # just starting point 
+#         dist = [10.0, 10.0]
 
-        while np.min(dist) < minDelta:
+#         while np.min(dist) < minDelta:
 
-            idx_point_x = idx_start_x.reshape((Ncells, Ncells, 1)) \
-                      + np.random.choice(idx_cell_x.reshape((-1,)), 
-                                         [Ncells, Ncells, Np])
+#             idx_point_x = idx_start_x.reshape((Ncells, Ncells, 1)) \
+#                       + np.random.choice(idx_cell_x.reshape((-1,)), 
+#                                          [Ncells, Ncells, Np])
 
-            idx_point_y = idx_start_y.reshape((Ncells, Ncells, 1)) \
-                      + np.random.choice(idx_cell_y.reshape((-1,)), 
-                                         [Ncells, Ncells, Np])
+#             idx_point_y = idx_start_y.reshape((Ncells, Ncells, 1)) \
+#                       + np.random.choice(idx_cell_y.reshape((-1,)), 
+#                                          [Ncells, Ncells, Np])
 
-            points_x = x_grid[idx_point_x, 0]
-            points_y = y_grid[0, idx_point_y]
+#             points_x = x_grid[idx_point_x, 0]
+#             points_y = y_grid[0, idx_point_y]
 
-            # we compute the periodic distance
+#             # we compute the periodic distance
 
-            diff_x = points_x.reshape((-1,1)) - points_x.reshape((-1,1)).T
-            diff_y = points_y.reshape((-1,1)) - points_y.reshape((-1,1)).T
+#             diff_x = points_x.reshape((-1,1)) - points_x.reshape((-1,1)).T
+#             diff_y = points_y.reshape((-1,1)) - points_y.reshape((-1,1)).T
 
-            # periodization
-            diff_x -= Ls*np.round(diff_x/Ls)
-            diff_y -= Ls*np.round(diff_y/Ls)
+#             # periodization
+#             diff_x -= Ls*np.round(diff_x/Ls)
+#             diff_y -= Ls*np.round(diff_y/Ls)
 
-            # computing the distance
-            dist = np.sqrt(np.square(diff_x) + np.square(diff_y))
-            dist += 10*np.eye(Ncells**2*Np)
+#             # computing the distance
+#             dist = np.sqrt(np.square(diff_x) + np.square(diff_y))
+#             dist += 10*np.eye(Ncells**2*Np)
 
 
-        pointsArray[i, :, 0] = x_grid[idx_point_x.reshape((-1,)), 0]
-        pointsArray[i, :, 1] = y_grid[0, idx_point_y.reshape((-1,))]
+#         pointsArray[i, :, 0] = x_grid[idx_point_x.reshape((-1,)), 0]
+#         pointsArray[i, :, 1] = y_grid[0, idx_point_y.reshape((-1,))]
 
-        R = pot[idx_point_x.reshape((-1,1)) 
-                        - idx_point_x.reshape((-1,1)).T, 
-                        idx_point_y.reshape((-1,1)) 
-                        - idx_point_y.reshape((-1,1)).T]
+#         R = pot[idx_point_x.reshape((-1,1)) 
+#                         - idx_point_x.reshape((-1,1)).T, 
+#                         idx_point_y.reshape((-1,1)) 
+#                         - idx_point_y.reshape((-1,1)).T]
 
-        RR = np.triu(R, 1)
-        potTotal = np.sum(RR)
+#         RR = np.triu(R, 1)
+#         potTotal = np.sum(RR)
 
-        potentialArray[i,:] = potTotal
+#         potentialArray[i,:] = potTotal
 
-        Fx = dpotdx[idx_point_x.reshape((-1,1)) 
-                           - idx_point_x.reshape((-1,1)).T, 
-                           idx_point_y.reshape((-1,1)) 
-                           - idx_point_y.reshape((-1,1)).T]
+#         Fx = dpotdx[idx_point_x.reshape((-1,1)) 
+#                            - idx_point_x.reshape((-1,1)).T, 
+#                            idx_point_y.reshape((-1,1)) 
+#                            - idx_point_y.reshape((-1,1)).T]
 
-        Fy = dpotdy[idx_point_x.reshape((-1,1)) 
-                           - idx_point_x.reshape((-1,1)).T, 
-                           idx_point_y.reshape((-1,1)) 
-                           - idx_point_y.reshape((-1,1)).T]
+#         Fy = dpotdy[idx_point_x.reshape((-1,1)) 
+#                            - idx_point_x.reshape((-1,1)).T, 
+#                            idx_point_y.reshape((-1,1)) 
+#                            - idx_point_y.reshape((-1,1)).T]
                                            
-        Fx = np.triu(Fx,1) + np.tril(Fx,-1)
-        Fy = np.triu(Fy,1) + np.tril(Fy,-1)
+#         Fx = np.triu(Fx,1) + np.tril(Fx,-1)
+#         Fy = np.triu(Fy,1) + np.tril(Fy,-1)
 
-        Forcesx = -np.sum(Fx, axis = 1) 
-        Forcesy = -np.sum(Fy, axis = 1) 
+#         Forcesx = -np.sum(Fx, axis = 1) 
+#         Forcesy = -np.sum(Fy, axis = 1) 
 
-        forcesArray[i,:,0] = Forcesx.T
-        forcesArray[i,:,1] = Forcesy.T
+#         forcesArray[i,:,0] = Forcesx.T
+#         forcesArray[i,:,1] = Forcesy.T
 
-    return pointsArray, potentialArray, forcesArray
+#     return pointsArray, potentialArray, forcesArray
 
