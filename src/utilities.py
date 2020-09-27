dd95b6ac2876aa4e17a31ec1a6c070f2022762f0
Forgotten <lzepeda@math.mit.edu>
2020-09-27 18:07:49 -0500

    examples: modify the example

--- src/utilities.py
+++ src/utilities.py
@@ -310,6 +310,46 @@ def genDistPerLongRangeFull(Rin, Ncells, Np, L, \
 
     return absInvList
 
+# @tf.function
+# def genDistVec1D(Rin, L, 
+#                           av = tf.constant([0.0, 0.0], dtype = tf.float32),
+#                           std =  tf.constant([1.0, 1.0], dtype = tf.float32)):
+#     # This function follows the same trick 
+#     # function to generate the generalized coordinates for periodic data
+#     # the input has dimensions (Nsamples, Ncells*Np)
+#     # the ouput has dimensions (Nsamples*Ncells*Np*(3*Np-1), 2)
+#     # we try to allocate an array before hand and use high level
+#     # tensorflow operations
+
+#     # neighList is a (Nsample, Npoints, maxNeigh)
+
+#     # add assert with respect to the input shape 
+#     Nsamples = Rin.shape[0]
+
+#     mask = neighList > -1
+
+#     RinRep  = tf.tile(tf.expand_dims(Rin, -1),[1 ,1,maxNumNeighs] )
+#     RinGather = tf.gather(Rin, neighList, batch_dims = 1, axis = 1)
+
+#     # substracting 
+#     R_Diff = RinGather - RinRep
+
+#     R_Diff = R_Diff - L*tf.round(R_Diff/L)
+
+#     bnorm = (tf.abs(R_Diff) - av[1])/std[1]
+#     binv = (tf.math.reciprocal(tf.abs(R_Diff)) - av[0])/std[0], 
+
+#     zeroDummy = tf.zeros_like(bnorm)
+
+#     bnorm_safe = tf.where(mask, bnorm, zeroDummy)
+#     binv_safe = tf.where(mask, binv, zeroDummy)
+    
+#     R_total = tf.concat([tf.reshape(binv_safe, (-1,1)), 
+#                               tf.reshape(bnorm_safe, (-1,1))], axis = 1)
+
+#     return R_total
+
+
 @tf.function
 def genDistInvLongRangeWindow(Rin, Ncells, Np, widht_tran = 0.5, width = 3.0, av = [0.0, 0.0], std = [1.0, 1.0]):
     # function to generate the generalized coordinates
